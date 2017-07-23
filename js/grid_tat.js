6132e0fd2620b5cf0f5179e02d5af751d635f790
Raja Reddy Karri <klnrajareddy@gmail.com>
2017-07-23 11:41:56 +0400

    Building Flow

---
+++ js/grid_tat.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+    var gridHeaders = [
+            "EMPLOYEE NAME",
+            "No. of Working Hrs",
+            "Cycle Time",
+            "Vol per Hr",
+            "Total Applications to be processed in Day",
+            "Actual Volumes",
+            "Avg TAT"
+        ];
+        var gridData = [
+            ["Shiv Shankar",8,15,14,32,33,"103"],
+            ["Emp2",8,15,14,32,34,"106"],
+            ["Emp3",8,15,14,32,25,"78"],
+            ["Emp4",8,15,14,32,26,"81"]
+        ];
+        var headerTemplate = $("#tableHeadTemplate").html();
+        var bodyTemplate = $("#tableBodyTemplate").html();
+        $("#tableHeader").html(_.template(headerTemplate)({items:gridHeaders}));
+        $("#tableBody").html(_.template(bodyTemplate)({rows:gridData}));
+        var chart = c3.generate({
+            bindto : "#chart",
+            data:{
+                x:'x',
+                columns :[
+                    ['x', '2016-01-01', '2016-02-01', '2016-03-01', '2016-04-01','2016-05-01'],
+                    ["Shiv Shankar",85,100,120,95,103],
+                    ["Emp2",100,105,79,85,106],
+                    ["Emp3",50,100,110,90,78],
+                    ["Emp4",81,100,50,95,85]
+                ]
+            },
+            axis: {
+            x: {
+                type: 'timeseries',
+                tick: {
+                    format: '%Y-%m'
+                }
+            }
+        }
+    });
+});
